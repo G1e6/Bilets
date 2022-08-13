0fb96fdc9cd4b7350634bb44fd48fb23c6540f29
G1e6 <gleb-potapov@rambler.ru>
2022-08-14 00:05:49 +0500

    compareTo

--- src/test/java/TicketsTest.java
+++ src/test/java/TicketsTest.java
@@ -64,7 +64,6 @@ public class TicketsTest {
         Tickets tickets4 = new Tickets(76, 500, "Пермь", "Пермь", 470);
 
 
-
         System.out.println(tickets1.compareTo(tickets2));
         System.out.println(tickets5.compareTo(tickets1));
         System.out.println(tickets3.compareTo(tickets4));
@@ -85,5 +84,20 @@ public class TicketsTest {
 
 
     }
+
+    @Test
+    public void compareTo() {
+
+        Tickets tickets5 = new Tickets(12, 2300, "Москва", "Казань", 95);
+        Tickets tickets1 = new Tickets(12, 2300, "Москва", "Казань", 95);
+        Tickets tickets2 = new Tickets(34, 6200, "Екатеринбург", "Москва", 150);
+        Tickets tickets3 = new Tickets(56, 11300, "Екатеринбург", "Сочи", 235);
+        Tickets tickets4 = new Tickets(76, 500, "Пермь", "Пермь", 470);
+
+        System.out.println(tickets1.compareTo(tickets5));
+        System.out.println(tickets1.compareTo(tickets2));
+        System.out.println(tickets3.compareTo(tickets5));
+    }
+
 
 }
